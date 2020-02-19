ee97d7c961b3e098f16d31282f8f9032009d9037
liuhui16 <liuhui16@100tal.com>
2020-02-19 13:59:40 +0800

    webpack打包文件

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,24 @@
 const path = require('path');
 
 module.exports = {
-  entry: './ui.js',  // 程序入口
+	mode: 'development',
+	target: "node",
+  entry: './cli.js',  // 程序入口
   output: {
     path: path.resolve(__dirname, 'dist'),   // 构建输出路径
     filename: 'cli.js',  // 构建输出的文件名
   },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        options: {
+          presets: ['@babel/preset-env', '@babel/preset-react'], // jsx转为js函数
+				},
+
+      },
+    ],
+	}
 };
