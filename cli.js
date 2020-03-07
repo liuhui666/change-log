8e0172e0fd22c0eed58c87f64a1dfe0316d81da5
liuhui16 <liuhui16@100tal.com>
2020-03-07 20:58:39 +0800

    打包版本

--- cli.js
+++ cli.js
@@ -1,9 +1,11 @@
-#!/usr/bin/env node
+import React from 'react';
+import { render } from 'ink';
+import ui from './ui'
 
-const React = require('react');// eslint-disable-line no-unused-vars
-const importJsx = require('import-jsx');
-const { render } = require('ink');
+// const React = require('react');// eslint-disable-line no-unused-vars
+// const importJsx = require('import-jsx');
+// const { render } = require('ink');
+// const ui = require('./ui');
 
-const ui = importJsx('./ui');
 
 render(ui);
