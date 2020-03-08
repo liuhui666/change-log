58e3af9b54960e8aec4b36ca89d85024dec4d199
liuhui16 <liuhui16@100tal.com>
2020-03-08 15:38:53 +0800

    删除无用内容

--- webpack.config.js
+++
@@ -1,9 +0,0 @@
-const path = require('path');
-
-module.exports = {
-  entry: './ui.js',  // 程序入口
-  output: {
-    path: path.resolve(__dirname, 'dist'),   // 构建输出路径
-    filename: 'cli.js',  // 构建输出的文件名
-  },
-};
