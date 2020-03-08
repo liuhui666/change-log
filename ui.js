aeb93be5dd25580fa36475d33a42ad998581e6a4
liuhui16 <liuhui16@100tal.com>
2020-03-08 15:38:22 +0800

    增加先找文件位置逻辑

--- ui.js
+++ ui.js
@@ -1,72 +1,89 @@
 'use strict'
 const React = require('react')
-const { useState } = require('react')
-const {  Box, Color, useInput, Text } = require('ink')
+const { useState, useEffect } = require('react')
+const { Box, Color, useInput, Text } = require('ink')
 const TextInput = require('ink-text-input')
-const writeFile = require('./index')
+const writeFile = require('./writeFile')
+const findFileDir = require('./findFile')
+
+let versionFileDir = null;
 
 const SearchQuery = () => {
-  const [order_num, changeNum] = useState(1)
-  const [logList, changeLogList] = useState([])
-  const [content, changeContent] = useState('')
-  useInput((input, key) => {
-    if (key.escape) {
-			if(!logList.length) return process.exit()
-      let result = ''
-      logList.forEach((item, index) => {
-        result += `\r\n\t${index + 1}. ${item}`
-      })
+	const [order_num, changeNum] = useState(1)
+	const [logList, changeLogList] = useState([])
+	const [content, changeContent] = useState('')
+	const [showContent, changeShowContent] = useState(false)
 
-      writeFile({
-        content: result,
-        url: './version.html'
-      }).then(() => {
-        process.exit()
-      })
+	useEffect(() => {
+		findFileDir().then((dir) => {
+			versionFileDir = dir;
+			changeShowContent(true)
+		}).catch(() => {
+			process.exit();
+		})
+	}, [])//第二个参数是空数组 相当于didMount
+
+	useInput((input, key) => {
+		if (key.escape) {
+			if (!logList.length) return process.exit()
+			let result = ''
+			logList.forEach((item, index) => {
+				result += `\r\n\t${index + 1}. ${item}`
+			})
+
+			writeFile({
+				content: result,
+				url: versionFileDir
+			}).then(() => {
+				process.exit()
+			})
 		}
 
 
-    if (key.return) {
-      // 按回车 换行 记录之前的输入
-      changeNum(order_num + 1)
-      changeLogList(() => {
-        logList.push(content)
-        return logList
-      })
-      changeContent('')
-    }
-  })
-  return (
-    <Box>
-      <div>
-        <Color white>请输入changelog内容 ：</Color>
-        <Color green>回车换行提交，Esc退出</Color>
-        <br />
-        <div>
-          {logList.map((item, index) => {
-            return (
-              <div key={index}>
-                <Text>
-                  {' '}
-                  {index + 1}. {item}
-                </Text>
-                <br />
-              </div>
-            )
-          })}
-        </div>
-        <Box>
-          <span>{order_num}.</span>
-          <TextInput.default
-            value={content}
-            onChange={value => {
-              changeContent(value)
-            }}
-          />
-        </Box>
-      </div>
-    </Box>
-  )
+		if (key.return) {
+			if (!content) return
+			// 按回车 换行 记录之前的输入
+			changeNum(order_num + 1)
+			changeLogList(() => {
+				logList.push(content)
+				return logList
+			})
+			changeContent('')
+		}
+	})
+	return (
+		<Box>
+			{
+				showContent ? <div>
+					<Color white>请输入changelog内容 ：</Color>
+					<Color green>回车换行提交，Esc退出</Color>
+					<br />
+					<div>
+						{logList.map((item, index) => {
+							return (
+								<div key={index}>
+									<Text>
+										{' '}
+										{index + 1}. {item}
+									</Text>
+									<br />
+								</div>
+							)
+						})}
+					</div>
+					<Box>
+						<span>{order_num}.</span>
+						<TextInput.default
+							value={content}
+							onChange={value => {
+								changeContent(value)
+							}}
+						/>
+					</Box>
+				</div> : <Color red>请先创建version文件</Color>
+			}
+		</Box>
+	)
 }
 
 module.exports = <SearchQuery />
